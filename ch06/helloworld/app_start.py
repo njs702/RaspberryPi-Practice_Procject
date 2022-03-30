1cb2c1627eb1923483170b4208433c391ef38cf0
junseok <njs702@ajou.ac.kr>
2022-03-30 19:58:33 +0900

    led control with flask server

--- ch06/helloworld/app_start.py
+++ ch06/helloworld/app_start.py
@@ -1,16 +1,29 @@
 from flask import Flask
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
+
+LED = 8
+GPIO.setmode(GPIO.BOARD) # BOARD connector pin number
+GPIO.setup(LED,GPIO.OUT,initial=GPIO.LOW)
+
 @app.route("/")
 def helloworld():
     return "Hello World!"
 
 @app.route("/led/on")
 def led_on():
+    GPIO.output(LED,GPIO.HIGH)
     return "LED ON"
+
+@app.route("/gpio/cleanup")
+def gpio_cleanup():
+    GPIO.cleanup()
+    return "GPIO CLEANUP"
 
 @app.route("/led/off")
 def led_off():
+    GPIO.output(LED,GPIO.LOW)
     return "LED OFF"
 
 
